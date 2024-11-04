b26a80e7657f62467d1dc961413c9122912edc8b
AndewRisc <zendy.dev@yahoo.com>
2024-11-05 04:37:05 +0700

    shop services added

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, Inject, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
 import { HttpClient } from '@angular/common/http';
-import { Product } from './shared/models/product';
 import { Pagination } from './shared/models/Pagination';
+import { Product } from './shared/models/Product';
+import { ShopService } from './core/service/shop.service';
+
 
 @Component({
   selector: 'app-root',
@@ -13,17 +15,15 @@ import { Pagination } from './shared/models/Pagination';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-  
-  baseUrl = 'https://localhost:5001/api/'
-  private http = inject(HttpClient);
+  private shopService = inject(ShopService)
   title = 'Skinet';
   products: Product[] = [];
 
   ngOnInit(): void {
-   this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
-    next: response => this.products = response.data,
-    error: error => console.log(error),
-    complete: () => console.log('complete')
+    this.shopService.getProduct().subscribe({
+      next: response => this.products = response.data,
+      error: error => console.log(error),
+      complete: () => console.log('complete')
    })
   }
 
