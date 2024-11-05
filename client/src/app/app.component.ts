1cba5324769a45d35a6e846ac77ef0cfbb454db8
AndewRisc <zendy.dev@yahoo.com>
2024-11-06 03:34:37 +0700

    shop ui

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,31 +2,21 @@ import { Component, Inject, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
 import { HttpClient } from '@angular/common/http';
-import { Pagination } from './shared/models/Pagination';
-import { Product } from './shared/models/Product';
+import { Pagination } from './shared/models/pagination';
+import { Product } from './shared/models/product';
 import { ShopService } from './core/service/shop.service';
+import { ShopComponent } from "./features/shop/shop.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ShopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-  private shopService = inject(ShopService)
+export class AppComponent {
   title = 'Skinet';
-  products: Product[] = [];
-
-  ngOnInit(): void {
-    this.shopService.getProduct().subscribe({
-      next: response => this.products = response.data,
-      error: error => console.log(error),
-      complete: () => console.log('complete')
-   })
-  }
-
   // constructor(private http: HttpClient){
   // }
 }
