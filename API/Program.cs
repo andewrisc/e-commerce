3225ee284e3c7ab8b1aa97bebec4f90d10c6b50f
AndewRisc <zendy.dev@yahoo.com>
2024-11-06 17:14:49 +0700

    add shopping cart and redis

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,12 @@
 using API.Middleware;
 using Core.Interface;
 using Infrastructure.Data;
-using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
 using Microsoft.EntityFrameworkCore;
+using Infrastructure;
+using StackExchange.Redis;
+using Core.Entities;
+using Infrastructure.Services;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +20,13 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddSingleton<IConnectionMultiplexer>(config => {
+    var connString = builder.Configuration.GetConnectionString("Redis") ?? throw new Exception("Cannot get redis connection string");
+    var configuration = ConfigurationOptions.Parse(connString, true);
+    return ConnectionMultiplexer.Connect(configuration);
+});
+builder.Services.AddSingleton<ICartService, CartService>();
+
 
 builder.Services.AddCors();
 
